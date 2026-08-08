f40a19bb6af5b90789a2d384356b6105f0e5523e
naoya hino <naoya@NaoyanoMacBook-Pro.local>
2026-08-08 15:28:21 +0900

    Implement answer submission

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2026_08_07_055911) do
+ActiveRecord::Schema[7.2].define(version: 2026_08_08_044051) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "answers", force: :cascade do |t|
+    t.text "content"
+    t.bigint "question_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["question_id"], name: "index_answers_on_question_id"
+  end
 
   create_table "questions", force: :cascade do |t|
     t.string "category"
@@ -20,4 +28,6 @@ ActiveRecord::Schema[7.2].define(version: 2026_08_07_055911) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  add_foreign_key "answers", "questions"
 end
