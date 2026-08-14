99ccee8241150cd000f4cf73a0497755f5d171cd
naoya hino <naoya@NaoyanoMacBook-Pro.local>
2026-08-15 02:40:15 +0900

    Implement post sns

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -8,7 +8,28 @@ class AnswersController < ApplicationController
     @question = Question.find(params[:question_id])
     @answer = @question.answers.create(answer_params)
 
-    redirect_to questions_show_path(category: @question.category)
+    redirect_to answer_path(@answer)
+  end
+
+  def show
+    @answer = Answer.find(params[:id])
+  end
+
+  def post_to_sns
+    @answer = Answer.find(params[:id])
+
+    @post_text = <<~TEXT
+      【お題】
+      #{@answer.question.content}
+
+      【回答】
+      #{@answer.content}
+
+      #大喜利
+    TEXT
+
+     redirect_to "https://twitter.com/intent/tweet?text=#{ERB::Util.url_encode(@post_text)}",
+              allow_other_host: true
   end
 
   private
